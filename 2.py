03b1eb17a894342016133c58e6c35ca7dfc7578d
princepatel1221 <patelkano1221@gmail.com>
2025-06-11 10:04:42 +0530

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,13 @@
+import math
+
+number = float(input("Enter a number: "))
+
+square_root = math.sqrt(number)
+
+natural_log = math.log(number)
+
+sine_value = math.sin(number)
+
+print("Square root:", square_root)
+print("logarithm:", natural_log)
+print("Sine:", sine_value)
